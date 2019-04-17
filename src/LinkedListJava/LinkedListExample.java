291879fc65527ee3444be8144151c022581730b5
fbayhan <fatih-bayhan@windowslive.com>
2019-04-18 00:18:58 +0300

    Linked list examples started

---
+++ src/LinkedListJava/LinkedListExample.java
@@ -0,0 +1,45 @@
+package LinkedListJava;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	private LinkedList<String> linkedList = new LinkedList<>();
+
+	public LinkedListExample() {
+		linkedList.add("First");
+		linkedList.add("Second");
+		linkedList.add("Third");
+		linkedList.add("Forth");
+
+	}
+
+	public void ReadLinkedListOnJava() {
+
+		Iterator<String> itr = linkedList.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+
+	public void addToLinkedList(String newItem) {
+		linkedList.add(newItem);
+		System.out.println(linkedList);
+	}
+
+	public void addToLinkedList(int place, String newItem) {
+		linkedList.add(place, newItem);
+		System.out.println(linkedList);
+	}
+
+	public void addToLinkedList(LinkedList<String> newLinkedList) {
+		linkedList.addAll(newLinkedList);
+		System.out.println(linkedList);
+	}
+
+	public void addToLinkedList(int place, LinkedList<String> newLinkedList) {
+		linkedList.addAll(place, newLinkedList);
+		System.out.println(linkedList);
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
+import java.util.LinkedList;
+
+import LinkedListJava.LinkedListExample;
 import tricks.Recursion;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to data structures");
-		
-		Recursion recursion=new Recursion();
-		System.out.println("Fibonacci : "+recursion.fibonacci(11));
-		System.out.println("Factorial : "+ recursion.factorial(4));
-		
-				
+
+		Recursion recursion = new Recursion();
+		System.out.println("Fibonacci : " + recursion.fibonacci(11));
+		System.out.println("Factorial : " + recursion.factorial(4));
+
+		LinkedListExample linkedListExample = new LinkedListExample();
+		linkedListExample.ReadLinkedListOnJava();
+		System.out.println("------------------------");
+		linkedListExample.addToLinkedList("newItem");
+		linkedListExample.ReadLinkedListOnJava();
+		System.out.println("------------------------");
+		linkedListExample.addToLinkedList(1, "New Second Item");
+		linkedListExample.ReadLinkedListOnJava();
+
+		System.out.println("------------------------");
+		LinkedList<String> newLinkedList = new LinkedList<>();
+		newLinkedList.add("First Item Of New Linked List");
+		newLinkedList.add("Second Item Of New Linked List");
+		newLinkedList.add("Third Item Of New Linked List");
+		newLinkedList.add("Fifth Item Of New Linked List");
+		linkedListExample.addToLinkedList(newLinkedList);
+		linkedListExample.ReadLinkedListOnJava();
+
+		System.out.println("------------------------");
+		LinkedList<String> newLinkedListWirhLink = new LinkedList<>();
+		newLinkedListWirhLink.add("First Item Of New Linked List");
+		newLinkedListWirhLink.add("Second Item Of New Linked List");
+		newLinkedListWirhLink.add("Third Item Of New Linked List");
+		newLinkedListWirhLink.add("Fifth Item Of New Linked List");
+		linkedListExample.addToLinkedList(2, newLinkedList);
+		linkedListExample.ReadLinkedListOnJava();
+
 	}
 }
